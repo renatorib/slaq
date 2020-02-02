1e7fe63aa01a77df97fb4b097e375d9a8d7f206c
Renato Ribeiro <renatoribroman@gmail.com>
2020-02-02 15:12:20 -0300

    chore: babel ignore test files

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  presets: ["@babel/preset-typescript", "@babel/preset-env"]
+  presets: ["@babel/preset-typescript", "@babel/preset-env"],
+  ignore: ["**/*.test.ts", "**/*.test.js"]
 };
