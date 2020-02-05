182bd9b48bdad8407b9b7b16265b831a788da6b4
Renato Ribeiro <renatoribroman@gmail.com>
2020-02-05 20:18:10 -0300

    fix(slaq): babel targets node 8

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  presets: ["@babel/preset-typescript", "@babel/preset-env"]
+  presets: [
+    "@babel/preset-typescript",
+    ["@babel/preset-env", { targets: { node: "8" } }]
+  ]
 };
